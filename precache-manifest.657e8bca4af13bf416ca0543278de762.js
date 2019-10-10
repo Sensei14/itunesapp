84f2fa408f31d820cd81f7e08bad54d7593dd3d0
unknown <karox1241@wp.pl>
2019-10-10 18:03:12 +0200

    Updates

---
+++ precache-manifest.657e8bca4af13bf416ca0543278de762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8304367b5c711c1c02ac5f357a3f3da",
+    "url": "/itunesapp/index.html"
+  },
+  {
+    "revision": "b2bcefff8dbf99709432",
+    "url": "/itunesapp/static/css/main.0c83ce94.chunk.css"
+  },
+  {
+    "revision": "f2a07db53a5e538fc6b2",
+    "url": "/itunesapp/static/js/2.ca97e2e1.chunk.js"
+  },
+  {
+    "revision": "b2bcefff8dbf99709432",
+    "url": "/itunesapp/static/js/main.9c3ca0f0.chunk.js"
+  },
+  {
+    "revision": "8c086b77134d3c72ae18",
+    "url": "/itunesapp/static/js/runtime-main.0aa05070.js"
+  },
+  {
+    "revision": "d3f670559a124db63a2e9ec824d7cb83",
+    "url": "/itunesapp/static/media/BG.d3f67055.jpg"
+  }
+]);
